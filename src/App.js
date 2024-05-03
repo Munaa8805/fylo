744796eb3efc21b53a2dbbc583705a0187c815f2
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2024-05-02 20:29:28 -0700

    create access

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Access from "./components/Access";
 import Features from "./components/Features";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -11,6 +12,7 @@ function App() {
       <Features />
       <Productive />
       <Testimonial />
+      <Access />
     </div>
   );
 }
